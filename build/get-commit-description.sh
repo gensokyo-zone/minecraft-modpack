ad5a9989855e7151e9ef24db23026b5937a6ac40
Kat Inskip <kat@inskip.me>
2024-10-13 19:37:06 -0400

    feat: commit vs release differentiation ^^

--- build/get-commit-description.sh
+++ build/get-commit-description.sh
@@ -5,7 +5,7 @@ git fetch --tags --force
 BRANCH=${GITHUB_REF:-$(git branch --show-current)}
 BRANCH=${BRANCH#refs/*/}
 echo 'TAG_SUBJECT<<EOF' >> $GITHUB_OUTPUT
-git log -n1 marka-1.20 --format="%h - %s" >> $GITHUB_OUTPUT
+git log -n1 marka-1.20 --format="Commit! %h - %s" >> $GITHUB_OUTPUT
 echo 'EOF' >> $GITHUB_OUTPUT
 echo 'TAG_BODY<<EOF' >> $GITHUB_OUTPUT
 git log -n1 marka-1.20 --format="%b" >> $GITHUB_OUTPUT
--- build/get-tag-description.sh
+++ build/get-tag-description.sh
@@ -5,7 +5,7 @@ git fetch --tags --force
 BRANCH=${GITHUB_REF:-$(git branch --show-current)}
 BRANCH=${BRANCH#refs/*/}
 echo 'TAG_SUBJECT<<EOF' >> $GITHUB_OUTPUT
-git tag -n1 "$BRANCH" --format=$'%(refname:lstrip=2) - %(contents:subject)' >> $GITHUB_OUTPUT
+git tag -n1 "$BRANCH" --format=$'Release! %(refname:lstrip=2) - %(contents:subject)' >> $GITHUB_OUTPUT
 echo 'EOF' >> $GITHUB_OUTPUT
 echo 'TAG_BODY<<EOF' >> $GITHUB_OUTPUT
 git tag -n1 "$BRANCH" --format=$'%(contents:body)' >> $GITHUB_OUTPUT
