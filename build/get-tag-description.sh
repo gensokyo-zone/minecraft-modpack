bc3ad4585ec3f032dc5601e75db09eb3370c9ff6
Kat Inskip <kat@inskip.me>
2024-10-13 19:24:28 -0400

    feat: tag description embed

--- build/get-tag-description.sh
+++ build/get-tag-description.sh
@@ -4,8 +4,10 @@ set -euxo pipefail
 git fetch --tags --force
 BRANCH=${GITHUB_REF:-$(git branch --show-current)}
 BRANCH=${BRANCH#refs/*/}
-echo 'TAG_DATA<<EOF' >> $GITHUB_OUTPUT
-git tag -n1 "$BRANCH" --format=$'# %(refname:lstrip=2) - %(contents:subject)\n\n%(contents:body)' >> $GITHUB_OUTPUT
-echo -n >> $GITHUB_OUTPUT
-echo "[Download](https://github.com/kittywitch/minecraft-modpack/releases/tag/${BRANCH})" >> $GITHUB_OUTPUT
+echo 'TAG_SUBJECT<<EOF' >> $GITHUB_OUTPUT
+git tag -n1 "$BRANCH" --format=$'%(refname:lstrip=2) - %(contents:subject)' >> $GITHUB_OUTPUT
 echo 'EOF' >> $GITHUB_OUTPUT
+echo 'TAG_BODY<<EOF' >> $GITHUB_OUTPUT
+git tag -n1 "$BRANCH" --format=$'%(contents:body)' >> $GITHUB_OUTPUT
+echo 'EOF' >> $GITHUB_OUTPUT
+echo "GIT_URL='https://github.com/kittywitch/minecraft-modpack/releases/tag/${BRANCH}'" >> $GITHUB_OUTPUT
