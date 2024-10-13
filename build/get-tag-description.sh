ac37d18b236b5a99c1711d8a5aa0186a9b3eb7e7
Kat Inskip <kat@inskip.me>
2024-10-13 18:33:02 -0400

    feat: fix webhook

--- build/get-tag-description.sh
+++ build/get-tag-description.sh
@@ -1,2 +1,2 @@
-TAG_DATA=$(git tag -n1 "$GITHUB_REF" --format=$'%(contents:subject)\n\n%(contents:body)')
-echo "TAG_NAME=${TAG_NAME}" >> $GITHUB_OUTPUT
+TAG_DATA=$(git tag -n1 "$GITHUB_REF" --format=$'# %(refname:lstrip=2) - %(contents:subject)\n%(contents:body)'
+echo "TAG_DATA=${TAG_DATA}" >> $GITHUB_OUTPUT
