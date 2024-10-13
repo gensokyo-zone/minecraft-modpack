471e0aadf2f20c5059906e661977e71315df410f
Kat Inskip <kat@inskip.me>
2024-10-13 19:33:58 -0400

    feat: regular commit webhooks

---
+++ build/get-commit-description.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+
+git fetch --tags --force
+BRANCH=${GITHUB_REF:-$(git branch --show-current)}
+BRANCH=${BRANCH#refs/*/}
+echo 'TAG_SUBJECT<<EOF' >> $GITHUB_OUTPUT
+git log -n1 marka-1.20 --format="%h - %s" >> $GITHUB_OUTPUT
+echo 'EOF' >> $GITHUB_OUTPUT
+echo 'TAG_BODY<<EOF' >> $GITHUB_OUTPUT
+git log -n1 marka-1.20 --format="%b" >> $GITHUB_OUTPUT
+echo 'EOF' >> $GITHUB_OUTPUT
+REF=$(git log -n1 marka-1.20 --format="%H")
+echo "TAG_URL=https://github.com/kittywitch/minecraft-modpack/commit/${REF}" >> $GITHUB_OUTPUT
