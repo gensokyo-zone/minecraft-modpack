d28b01b46bc10a8ca8cf0a5c215e1f885e0d825f
Kat Inskip <kat@inskip.me>
2024-10-13 18:50:15 -0400

    feat: multi-line output...?

--- build/get-tag-description.sh
+++ build/get-tag-description.sh
@@ -1,2 +1,4 @@
 TAG_DATA=$(git tag -n1 "$GITHUB_REF" --format=$'# %(refname:lstrip=2) - %(contents:subject)\n%(contents:body)')
-echo "TAG_DATA=${TAG_DATA}" >> $GITHUB_OUTPUT
+echo 'TAG_DATA<<EOF' >> $GITHUB_OUTPUT
+echo "$TAG_DATA" >> $GITHUB_OUTPUT
+echo 'EOF' >> $GITHUB_OUTPUT
