6d9cb1b339ea270fe28934ac00242f5c8e655298
Kat Inskip <kat@inskip.me>
2024-10-13 18:55:41 -0400

    feat: wow somebody interrupted me and i forgot that part

--- build/get-tag-description.sh
+++ build/get-tag-description.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-TAG_DATA=$(git tag -n1 "$GITHUB_REF" --format=$'# %(refname:lstrip=2) - %(contents:subject)\n%(contents:body)')
+BRANCH=${GITHUB_REF:-$(git branch --show-current)}
+BRANCH=${BRANCH#refs/*/}
+
+TAG_DATA=$(git tag -n1 "$BRANCH" --format=$'# %(refname:lstrip=2) - %(contents:subject)\n%(contents:body)')
 echo 'TAG_DATA<<EOF' >> $GITHUB_OUTPUT
 echo "$TAG_DATA" >> $GITHUB_OUTPUT
 echo 'EOF' >> $GITHUB_OUTPUT
