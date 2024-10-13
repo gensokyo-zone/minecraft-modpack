97677c25b06c817cec7e92fe182822f4e771be25
Kat Inskip <kat@inskip.me>
2024-10-13 19:13:01 -0400

    feat: ??? github is made by idiots i guess

--- build/get-tag-description.sh
+++ build/get-tag-description.sh
@@ -3,7 +3,8 @@ set -euxo pipefail
 
 BRANCH=${GITHUB_REF:-$(git branch --show-current)}
 BRANCH=${BRANCH#refs/*/}
-
 echo 'TAG_DATA<<EOF' >> $GITHUB_OUTPUT
 git tag -n1 "$BRANCH" --format=$'# %(refname:lstrip=2) - %(contents:subject)\n\n%(contents:body)' >> $GITHUB_OUTPUT
+echo -n >> $GITHUB_OUTPUT
+echo "[Download](https://github.com/kittywitch/minecraft-modpack/releases/tag/${BRANCH})" >> $GITHUB_OUTPUT
 echo 'EOF' >> $GITHUB_OUTPUT
