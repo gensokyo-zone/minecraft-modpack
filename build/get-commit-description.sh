74ca94f1bdb15a8dc7628de06964c4f9a0f5f114
Kat Inskip <kat@inskip.me>
2024-10-13 19:41:15 -0400

    fix: variable names were wrong

--- build/get-commit-description.sh
+++ build/get-commit-description.sh
@@ -3,11 +3,11 @@ set -euxo pipefail
 
 BRANCH=${GITHUB_REF:-$(git branch --show-current)}
 BRANCH=${BRANCH#refs/*/}
-echo 'TAG_SUBJECT<<EOF' >> $GITHUB_OUTPUT
+echo 'COMMIT_SUBJECT<<EOF' >> $GITHUB_OUTPUT
 git log -n1 ${BRANCH} --format="Commit! %h - %s" >> $GITHUB_OUTPUT
 echo 'EOF' >> $GITHUB_OUTPUT
-echo 'TAG_BODY<<EOF' >> $GITHUB_OUTPUT
+echo 'COMMIT_BODY<<EOF' >> $GITHUB_OUTPUT
 git log -n1 ${BRANCH} --format="%b" >> $GITHUB_OUTPUT
 echo 'EOF' >> $GITHUB_OUTPUT
 REF=$(git log -n1 ${BRANCH} --format="%H")
-echo "TAG_URL=https://github.com/kittywitch/minecraft-modpack/commit/${REF}" >> $GITHUB_OUTPUT
+echo "COMMIT_URL=https://github.com/kittywitch/minecraft-modpack/commit/${REF}" >> $GITHUB_OUTPUT
