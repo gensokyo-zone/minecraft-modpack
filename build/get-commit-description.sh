1824abbbcbb602c37563881f3973c616b4784c0a
Kat Inskip <kat@inskip.me>
2024-10-13 19:38:25 -0400

    feat: use branch

--- build/get-commit-description.sh
+++ build/get-commit-description.sh
@@ -5,10 +5,10 @@ git fetch --tags --force
 BRANCH=${GITHUB_REF:-$(git branch --show-current)}
 BRANCH=${BRANCH#refs/*/}
 echo 'TAG_SUBJECT<<EOF' >> $GITHUB_OUTPUT
-git log -n1 marka-1.20 --format="Commit! %h - %s" >> $GITHUB_OUTPUT
+git log -n1 ${BRANCH} --format="Commit! %h - %s" >> $GITHUB_OUTPUT
 echo 'EOF' >> $GITHUB_OUTPUT
 echo 'TAG_BODY<<EOF' >> $GITHUB_OUTPUT
-git log -n1 marka-1.20 --format="%b" >> $GITHUB_OUTPUT
+git log -n1 ${BRANCH} --format="%b" >> $GITHUB_OUTPUT
 echo 'EOF' >> $GITHUB_OUTPUT
-REF=$(git log -n1 marka-1.20 --format="%H")
+REF=$(git log -n1 ${BRANCH} --format="%H")
 echo "TAG_URL=https://github.com/kittywitch/minecraft-modpack/commit/${REF}" >> $GITHUB_OUTPUT
