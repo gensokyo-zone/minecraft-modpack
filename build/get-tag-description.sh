208e54604f68933948bb354f3c2ff721f604fcf2
Kat Inskip <kat@inskip.me>
2024-10-13 18:59:04 -0400

    feat: hmmm it stopped after a newline, annoying

--- build/get-tag-description.sh
+++ build/get-tag-description.sh
@@ -4,7 +4,6 @@ set -euxo pipefail
 BRANCH=${GITHUB_REF:-$(git branch --show-current)}
 BRANCH=${BRANCH#refs/*/}
 
-TAG_DATA=$(git tag -n1 "$BRANCH" --format=$'# %(refname:lstrip=2) - %(contents:subject)\n%(contents:body)')
 echo 'TAG_DATA<<EOF' >> $GITHUB_OUTPUT
-echo "$TAG_DATA" >> $GITHUB_OUTPUT
+git tag -n1 "$BRANCH" --format=$'# %(refname:lstrip=2) - %(contents:subject)\n\n%(contents:body)' >> $GITHUB_OUTPUT
 echo 'EOF' >> $GITHUB_OUTPUT
